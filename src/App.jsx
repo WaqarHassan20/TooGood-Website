37a63a3690b8d8a47097baba628b20cc3acacb87
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-30 01:09:53 +0500

    Too Good Website Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import HandyMan from "./components/HandyMan";
+import Journal from "./components/Journal";
+import Navbar from "./components/Navbar";
+import TableChairImg from "./components/TableChairImg";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HandyMan />
+      <TableChairImg />
+      <Journal />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
